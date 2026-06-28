4baa7ed6d31b9f028181ed33e30cd8cdd47156b0
Kisaufaa Mecca Andiartha <meccamandarin@gmail.com>
2026-06-28 22:43:52 +0800

    Menambah efek ToggleDown pada NavProfile Arrow

--- src/components/molecules/NavProfile.jsx
+++ src/components/molecules/NavProfile.jsx
@@ -1,13 +1,39 @@
+import { useState } from 'react'
 import Avatar from '../atoms/Avatar'
 
 function NavProfile({src}) {
+
+    const [isOpen, setIsOpen] = useState(false)
+
+    const toggleDropdown = () => {
+        setIsOpen(!isOpen)
+    }
+
     return(
-    
-        <div className="flex items-center gap-2 cursor-pointer">
-            <Avatar src={src} className=""/>
-            <img src="/arrow/arrow-down.png" alt="memuat" className="w-4 h-4 invert"/>
-        </div>
         
+    <div className='relative inline-block text-left'>
+
+        <div className="flex items-center gap-2 cursor-pointer" onClick={toggleDropdown}>
+
+            <Avatar src={src} className=""/>
+
+            <img 
+            src="/arrow/arrow-down.png" 
+            alt="memuat" 
+            className={`w-4 h-4 invert 
+            -rotate-90 transition-transform duration-200
+            ${isOpen ? 'rotate-0' : '-rotate-90'}`}/>
+        </div>
+
+        {isOpen && (
+        <div className='absolute right-0 mt-3 w-28 md:w-30 text-white bg-[#181A1C] rounded-md shadow-lg py-2 z-999999 border border-gray-900 '>
+            <a href="/login" className="block px-4 py-2 text-sm hover:text-blue-600">Login</a>
+            <a href="/register" className="block px-4 py-2 text-sm hover:text-blue-600">Registrasi</a>
+            <a href="/mylist" className="block px-4 py-2 text-sm hover:text-blue-600">Daftar Saya</a>
+        </div>
+        )}
+
+    </div>
     )
 }
 
--- src/pages/MyListPage.jsx
+++ src/pages/MyListPage.jsx
@@ -1,9 +1,19 @@
 function MyListPage() {
     return (
-        <>
-        <h1>Ini List Saya</h1>
-        </>
+        <div className="
+        py-8 px-8 md:px-12 relative group/section
+        w-full min-h-screen
+        bg-cover bg-center bg-[#181A1C]
+        text-white
+        flex">
+        <h1 className="text-lg md:text-2xl font-semibold mb-4 tracking-wide">Daftar Saya</h1>
+
+        
+        </div>
     )
 }
+
+      
+    
 
 export default MyListPage
